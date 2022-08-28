b0b65d624f80f206c110e475d7583028101f0684
waqas <105483652+waqas-on-github@users.noreply.github.com>
2022-08-29 02:48:12 +0500

    basic sql with python

--- CRUD.py
+++ CRUD.py
@@ -11,39 +11,35 @@ cur.execute("create database IF NOT EXISTS  db1") # executing instrucations  cre
 students="CREATE TABLE  IF NOT EXISTS  students(name VARCHAR(30) ,email TEXT ,age int)" # creating tabel 
 cur.execute(students)
 
-# i ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  # inserting data into tabel 
-# d=("waqas" ,'mrwaqas@gmail.com' ,25)  
-# cur.execute(i,d)
-# db.commit()
+i ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  # inserting data into tabel 
+d=("waqas" ,'mrwaqas@gmail.com' ,25)  
+cur.execute(i,d)
+db.commit()
 
 # inserting multiple recods in tabel 
-# ix ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  # inserting data into tabel 
-# dr=[("'usman'" ,"'me@gm'" ,26),("imran","mr@gm",28),("huzaifa","hzu@m",29 )]
-# cur.executemany(ix,dr)
-# db.commit() 
+ix ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  # inserting data into tabel 
+dr=[("'usman'" ,"'me@gm'" ,26),("imran","mr@gm",28),("huzaifa","hzu@m",29 )]
+cur.executemany(ix,dr)
+db.commit() 
 
 
 
 # reading data 
-
-
-
-
-# update data 
-# cmd ="UPDATE students SET age=age+1 WHERE age >20"
-# cur.execute(cmd)
-# db.commit()
-
-
-
-
-
-delete="DELETE FROM  students WHERE age >35"
-cur.execute(delete)
-db.commit()
 
 rd="select * from students"
 cur.execute(rd)
 result =cur.fetchall()
 for r in result:
     print(r)
+
+# update data 
+cmd ="UPDATE students SET age=age+1 WHERE age >20"
+cur.execute(cmd)
+db.commit()
+
+
+# delete data 
+delete="DELETE FROM  students WHERE age >35"
+cur.execute(delete)
+db.commit()
+
