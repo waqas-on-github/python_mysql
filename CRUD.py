53e0b56b70859c527adafae0c752c1b0f8e54b36
waqas <105483652+waqas-on-github@users.noreply.github.com>
2022-08-29 02:50:14 +0500

    basic sql with python

--- CRUD.py
+++ CRUD.py
@@ -2,30 +2,31 @@
 import mysql.connector   # importing db connector    
 
 db= mysql.connector.connect(host="localhost" , user ="root" ,password ="waqasx@#")   # connecting with db 
-print(db.connection_id)  # check connection 
+# check connection 
+print(db.connection_id)  
 
-cur =db.cursor() # creating cursor object for  opreation  in db 
+# creating cursor object for  opreation  in db 
+cur =db.cursor() 
 cur.execute("use db1")
 
-cur.execute("create database IF NOT EXISTS  db1") # executing instrucations  creating db  
+# executing instrucations  creating db  
+cur.execute("create database IF NOT EXISTS  db1") 
 students="CREATE TABLE  IF NOT EXISTS  students(name VARCHAR(30) ,email TEXT ,age int)" # creating tabel 
 cur.execute(students)
 
-i ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  # inserting data into tabel 
+# inserting data into tabel 
+i ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  
 d=("waqas" ,'mrwaqas@gmail.com' ,25)  
 cur.execute(i,d)
 db.commit()
 
 # inserting multiple recods in tabel 
-ix ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)"  # inserting data into tabel 
+ix ="INSERT INTO students (name,email,age) VALUES (%s,%s,%s)" 
 dr=[("'usman'" ,"'me@gm'" ,26),("imran","mr@gm",28),("huzaifa","hzu@m",29 )]
 cur.executemany(ix,dr)
 db.commit() 
 
-
-
 # reading data 
-
 rd="select * from students"
 cur.execute(rd)
 result =cur.fetchall()
@@ -36,7 +37,6 @@ for r in result:
 cmd ="UPDATE students SET age=age+1 WHERE age >20"
 cur.execute(cmd)
 db.commit()
-
 
 # delete data 
 delete="DELETE FROM  students WHERE age >35"
